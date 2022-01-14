9d9fad0e438bb371d0e51613ff9c8552a2237ce2
harpriya19 <90307264+harpriya19@users.noreply.github.com>
2022-01-14 15:04:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+music1 = "";
+music2 = "";
+
+function preload()
+{
+    music1 = loadSound("music.mp3");
+    music2 = loadSound("music2.mp3");
+}
+function setup()
+{
+    canvas = createCanvas(550,650);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+    image(video, 0, 0,550, 650);
+}
